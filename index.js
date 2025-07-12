fdc46d78160cb71ae14f9cf1643ffa80b376606a
Moataz <moataz_abdullah@outlook.com>
2025-07-12 12:21:09 +0300

    modified the loadingspinner visually and created the logic for it

--- index.js
+++ index.js
@@ -2,14 +2,24 @@ import UI from "./ui.js";
 import { getGames } from "./home.js";
 import themeSwitch from "./themeUI.js";
 themeSwitch();
+function showSpinner() {
+  document.getElementById("loadingSpinner").classList.remove("d-none");
+}
+
+function hideSpinner() {
+  document.getElementById("loadingSpinner").classList.add("d-none");
+}
 document.addEventListener("DOMContentLoaded", async () => {
   const ui = new UI(".box");
 
   try {
+    showSpinner();
     const games = await getGames();
     ui.displayData(games);
   } catch (err) {
     console.error("Failed to fetch games:", err);
+  } finally {
+    setTimeout(() => hideSpinner(), 300);
   }
   const links = document.querySelectorAll(".nav-link");
 
@@ -18,10 +28,13 @@ document.addEventListener("DOMContentLoaded", async () => {
       const category = this.getAttribute("data-category");
 
       try {
+        showSpinner();
         const games = await getGames(category);
         ui.displayData(games);
       } catch (err) {
         console.error(`Failed to fetch games for ${category}:`, err);
+      } finally {
+        setTimeout(() => hideSpinner(), 300);
       }
     });
   });
